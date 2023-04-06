0bbe024ce6f16584e7435ee734375843ff87c51f
Rohit <rbhardw1@unb.ca>
2023-04-06 19:43:04 -0300

    updated PrimsAlgo class

--- src/PrimsAlgo.java
+++ src/PrimsAlgo.java
@@ -12,54 +12,69 @@ public class PrimsAlgo {
     public void minimumSpanningTree() {
         Node node = null;
 
-//        ArrayList<String> visited = new ArrayList<>();
+        ArrayList<Node> visited = new ArrayList<>();
         ArrayList<Node> temp = new ArrayList<>();
         String currentEdge = "";
         int counter = 0;
+        boolean breakWhile = false;
         currentEdge = startingEdge;
         while(counter < graph.size()) {
             boolean added = false;
             for(Node edge : graph) {
                 if(edge.getSource().equalsIgnoreCase(currentEdge)) {
-                    temp.add(edge);
-                    added = true;
-//                    visited.add(edge.getSource());
-                    counter++;
-                }
-
-            }
-            if(!added) {
-                for(Node edge : graph) {
-                    if (edge.getDestination().equalsIgnoreCase(currentEdge)) {
+                    boolean visit = false;
+                    for(Node vst : visited) {
+                        if(vst.getSource().equalsIgnoreCase(edge.getSource()) && vst.getDestination().equalsIgnoreCase(edge.getDestination())) {
+                            visit = true;
+                        }
+                    }
+                    if(!visit) {
                         temp.add(edge);
-
+                        added = true;
 //                    visited.add(edge.getSource());
                         counter++;
                     }
+                    else {
+                        currentEdge = edge.getSource();
+                        breakWhile = true;
+                    }
                 }
-            }
-            int min = 0;
 
-            for(Node edge : temp){
-                if(min == 0) {
-                    min = edge.getWeight();
-                    node = edge;
+            }
+
+                if (!added) {
+                    for (Node edge : graph) {
+                        if (edge.getDestination().equalsIgnoreCase(currentEdge)) {
+                            temp.add(edge);
+
+//                    visited.add(edge.getSource());
+                            counter++;
+                        }
+                    }
                 }
-                else if(min > edge.getWeight()) {
-                    min = edge.getWeight();
-                    node = edge;
+                int min = 0;
+
+                for (Node edge : temp) {
+                    if (min == 0) {
+                        min = edge.getWeight();
+                        node = edge;
+                    } else if (min > edge.getWeight()) {
+                        min = edge.getWeight();
+                        node = edge;
+                    }
+                }
+                if (added) {
+                    System.out.println(node);
+                    visited.add(node);
+                    currentEdge = node.getDestination();
+                    temp.remove(node);
+                } else {
+                    System.out.println(node);
+                    visited.add(node);
+                    currentEdge = node.getSource();
+                    temp.remove(node);
                 }
             }
-            if(added) {
-                System.out.println(node);
-                currentEdge = node.getDestination();
-                temp.remove(node);
-            }
-            else {
-                System.out.println(node);
-                currentEdge = node.getSource();
-                temp.remove(node);
-            }
-        }
+
     }
 }
